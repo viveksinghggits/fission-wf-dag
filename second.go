c45a5429b94e2836920c5ce298505d310ab661c6
Vivek Sinh <vivek@infracloud.io>
2018-11-23 14:27:44 +0530

    working for all the functions

--- second.go
+++ second.go
@@ -4,10 +4,19 @@ package main
 
 import (
     "net/http"
+    "io/ioutil"
 )
 
 // Handler is the entry point for this fission function
 func Handler(w http.ResponseWriter, r *http.Request) {
-    msg := "Second Hello, world!\n"
-    w.Write([]byte(msg))
+	reqBody, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte(err.Error()))
+    }
+
+    body := string(reqBody)
+    finalres:=body+"second funciton\n"
+
+    w.Write([]byte(finalres))
 }
